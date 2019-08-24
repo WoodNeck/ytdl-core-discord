63a68d1a9752c630baf8c9daa6008042f6d1bc24
WoodNeck <saiyu915@naver.com>
2019-08-24 21:06:36 +0900

    fix: missing typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="node" />
 import { Readable } from 'stream';
-import { downloadOptions, videoFormat } from 'ytdl-core';
+import { downloadOptions, videoFormat, videoInfo } from 'ytdl-core';
 export interface PrismVideoFormat extends videoFormat {
 	audio_sample_rate?: number;
 }
-export declare function download(url: string, options?: downloadOptions): Promise<Readable>;
+export declare function download(url: string, options?: downloadOptions): Promise<[Readable, videoInfo]>;
